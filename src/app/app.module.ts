6d5f3a6300bb17c081b15aaf848d6bc9b176bd84
imolinuevo <i.m.parron@opendeusto.es>
2018-04-15 12:35:44 +0200

    End tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { Store, StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
-
+import { PizzaModule } from './pizza/pizza.module';
+import { reducers } from './reducers';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    }),
+    PizzaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pizza/pizza.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PizzaOrderComponent } from './pizza-order/pizza-order.component';
+import { Store, StoreModule } from '@ngrx/store';
+import { pizzaReducer } from './pizza.reducer';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    StoreModule.forFeature('pizza', pizzaReducer)
+  ],
+  exports: [PizzaOrderComponent],
+  declarations: [PizzaOrderComponent]
+})
+export class PizzaModule { }
